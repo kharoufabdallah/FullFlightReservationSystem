4d54a41f10bd2623b623a2cefcb3e2fd9c5fe821
kharoufabdallah <48812005+kharoufabdallah@users.noreply.github.com>
2020-05-14 11:48:31 +0300

    Add files via upload

--- Seat.java
+++ Seat.java
@@ -1,62 +1,72 @@
 package ASSI4;
 //Abdallah Kharouf 1183328 Lab_4
 
-import java.util.GregorianCalendar;
+public abstract class Seat {
+	private int rowNumber;
+	private int colNumber;
+	private String seatNumber;
+	protected Passenger passenger;   //Not private passenger in order to give access easily in the same directory //
+	boolean reserved = false;
 
-public class Passenger extends Person{
-
-	private long pass_no;
-	private String nationality;
-	private boolean has_visa;
-	private String seat_type= "Aisle";
-
-
-	public Passenger(String firstName, String lastName, char gender, GregorianCalendar dob, long pass_no, String nationality, boolean has_visa, String seat_type) {
-		super(firstName, lastName,gender, dob);
-		this.pass_no = pass_no;
-		this.nationality = nationality;       //No no-arg's constructor because all of these data members are important to be provided//
-		this.has_visa = has_visa;
-		this.seat_type = seat_type;
+	public Seat() {
 	}
 
-	public long getPass_no() {
-		return pass_no;
+	public Seat(int rowNumber, int colNumber, String seatNumber, Passenger passenger, boolean reserved) {
+		this.rowNumber = rowNumber;
+		this.colNumber = colNumber;
+		this.seatNumber = seatNumber;
+		this.passenger = passenger;
+		this.reserved = reserved;
 	}
 
-	public void setPass_no(long pass_no) {
-		this.pass_no = pass_no;
+
+	public int getRowNumber() {
+		return rowNumber;
 	}
 
-	public String getNationality() {
-		return nationality;
+	public void setRowNumber(int rowNumber) {
+		this.rowNumber = rowNumber;
 	}
 
-	public void setNationality(String nationality) {
-		this.nationality = nationality;
+	public int getColNumber() {
+		return colNumber;
 	}
 
-	public boolean isHas_visa() {
-		return has_visa;
+	public void setColNumber(int colNumber) {
+		this.colNumber = colNumber;
 	}
 
-	public void setHas_visa(boolean has_visa) {
-		this.has_visa = has_visa;
+	public String getSeatNumber() {
+		return seatNumber;
 	}
 
-	public String getSeat_type() {
-		return seat_type;
+	public void setSeatNumber(String seatNumber) {
+		this.seatNumber = seatNumber;
 	}
 
-	public void setSeat_type(String seat_type) {
-		this.seat_type = seat_type;
+	public Passenger getPassenger() {
+		return passenger;
 	}
 
-	public void prints() {
-		System.out.println(super.toString()+  //inheritance rule! ..//
-				  "\n pass_no=" + pass_no +"\n"+
-				  ", nationality='" + nationality + "\n" +
-				  ", visa=" + has_visa + "\n" +
-				  ", seat_type='" + seat_type + "\n" +
-				  '}');
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
 	}
+
+	public boolean isReserved() {
+		return reserved;
+	}
+
+	public void setReserved(boolean reserved) {
+		this.reserved = reserved;
+	}
+
+	@Override
+	public String toString() {
+		return "Seat{" +"\n"+
+				  "rowNumber=" + rowNumber + "\n"+
+				  ", colNumber=" + colNumber + "\n" +
+				  ", passenger=" + passenger +
+				  '}';
+	}
+
 }
--- Trip.java
+++ Trip.java
@@ -1,52 +1,115 @@
 package ASSI4;
 //Abdallah Kharouf 1183328 Lab_4
 
-public class FirstClass extends Seat{
-	private String foodPreferF = "Caviar";
-	private String drinkPreferF = "Liquore";
-	private boolean reserved = false;
+import java.util.Arrays;
+import java.util.GregorianCalendar;  // We have to make an Array_list of Trips differs than the
+                                     // ultimate ragged array of seats.
+                                     //like the previous assignment, we have to add the seats
+                                     // array in the arguments of the new Trip Object in the created Arra_list//
 
-	public FirstClass() {
+	public class Trip implements Comparable<Trip>{ //Using interface//
+		protected Seat[][] seat_trip; //ragged array of seats as a data member
+		private String flight_no;
+		private String to_air;
+		private String f_air;
+		private double distance;
+		private GregorianCalendar dip_time;
+		private GregorianCalendar ar_time;
 
+
+		public Trip(Seat[][] seat_trip, String flight_no, String f_air, String to_air, double distance, GregorianCalendar dip_time, GregorianCalendar ar_time) {
+			this.seat_trip = seat_trip;
+			this.flight_no = flight_no;
+			this.f_air = f_air;
+			this.to_air = to_air;
+			this.distance = distance;
+			this.dip_time = dip_time;
+			this.ar_time = ar_time;
+		}
+
+		public Seat[][] getSeat_trip() {
+			return seat_trip;
+		}
+
+		public void setSeat_trip(Seat[][] seat_trip) {
+			this.seat_trip = seat_trip;
+		}
+
+		public String getFlight_no() {
+			return flight_no;
+		}
+
+		public void setFlight_no(String flight_no) {
+			this.flight_no = flight_no;
+		}
+
+		public String getF_air() {
+			return f_air;
+		}
+
+		public void setF_air(String f_air) {
+			this.f_air = f_air;
+		}
+
+		public String getTo_air() {
+			return to_air;
+		}
+
+		public void setTo_air(String to_air) {
+			this.to_air = to_air;
+		}
+
+		public double getDistance() {
+			return distance;
+		}
+
+		public void setDistance(double distance) {
+			this.distance = distance;
+		}
+
+		public GregorianCalendar getDip_time() {
+			return dip_time;
+		}
+
+		public void setDip_time(GregorianCalendar dip_time) {
+			this.dip_time = dip_time;
+		}
+
+		public GregorianCalendar getAr_time() {
+			return ar_time;
+		}
+
+		public void setAr_time(GregorianCalendar ar_time) {
+			this.ar_time = ar_time;
+		}
+
+		public void prints() {
+			System.out.println( "Trip :" +
+					  ",\n flight number # ='" + flight_no + '\'' +
+					  ",\n from airport='" + f_air + '\'' +
+					  ",\n to airport='" + to_air + '\'' +
+					  ",\n distance=" + distance +
+					  ",\n diparture time=" + dip_time +
+					  ",\n arrival time=" + ar_time
+			);
+		}
+
+		@Override
+		public String toString() {
+			return "Trip{" +
+					  "seat_trip=" + Arrays.toString(seat_trip) +
+					  ", flight_no='" + flight_no + '\'' +
+					  ", to_air='" + to_air + '\'' +
+					  ", f_air='" + f_air + '\'' +
+					  ", distance=" + distance +
+					  ", dip_time=" + dip_time +
+					  ", ar_time=" + ar_time +
+					  '}';
+		}
+
+		@Override
+		public int compareTo(Trip trip) {
+			return 0;
+		}
 	}
 
-	public FirstClass(int rowNumber, int colNumber, String seatNumber, Passenger passenger, boolean reserved, String foodPreferF, String drinkPreferF, boolean reserved1) {
-		super(rowNumber, colNumber, seatNumber, passenger, reserved);
-		this.foodPreferF = foodPreferF;
-		this.drinkPreferF = drinkPreferF;
-		this.reserved = reserved1;
-	}
-
-	public String getFoodPreferF() {
-		return foodPreferF;
-	}
-
-	public void setFoodPreferF(String foodPreferF) {
-		this.foodPreferF = foodPreferF;
-	}
-
-	public String getDrinkPreferF() {
-		return drinkPreferF;
-	}
-
-	public void setDrinkPrefer(String drinkPreferF) {
-		this.drinkPreferF = drinkPreferF;
-	}
-
-	@Override
-	public boolean isReserved() {
-		return reserved;
-	}
-
-	@Override
-	public void setReserved(boolean reserved) {
-		this.reserved = reserved;
-	}
-
-	@Override
-	public String toString() {
-		return "FirstClass{" + super.toString() +"\n"+
-				  "foodPreferF='" + foodPreferF + "\n" +
-				  ", drinkPreferF='" + drinkPreferF ;
-	}
-}
